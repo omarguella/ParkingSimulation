f0c76dff58d0cb21af5a99b8bfa794e4987b93ce
omarguella <83080774+omarguella@users.noreply.github.com>
2021-07-13 15:11:24 +0100

    Read from Body

--- ParkhausSimulation-projekt/src/main/java/servlets/ParkhausServlet.java
+++ ParkhausSimulation-projekt/src/main/java/servlets/ParkhausServlet.java
@@ -38,4 +38,38 @@ public class ParkhausServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
+
+	private ServletContext getApplication() {
+		return getServletConfig().getServletContext();
+	}
+
+
+
+
+
+
+private static String getBody(HttpServletRequest request) throws IOException {
+	StringBuilder stringBuilder = new StringBuilder();
+	BufferedReader bufferedReader = null;
+	try {
+		InputStream inputStream = request.getInputStream();
+		if (inputStream != null) {
+			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			char[] charBuffer = new char[128];
+			int bytesRead = -1;
+			while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+				stringBuilder.append(charBuffer, 0, bytesRead);
+			}
+		} else {
+			stringBuilder.append("");
+		}
+	} finally {
+		if (bufferedReader != null) {
+			bufferedReader.close();
+		}
+	}
+	return stringBuilder.toString();
+
+}
+
 }
