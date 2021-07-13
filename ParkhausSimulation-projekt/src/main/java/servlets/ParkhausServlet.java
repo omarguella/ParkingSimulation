da8c6dee42a5c7b22acec7882cb8c12e73b530d2
omarguella <83080774+omarguella@users.noreply.github.com>
2021-07-13 15:33:33 +0100

    //test

--- ParkhausSimulation-projekt/src/main/java/servlets/ParkhausServlet.java
+++ ParkhausSimulation-projekt/src/main/java/servlets/ParkhausServlet.java
@@ -98,56 +98,7 @@ public class ParkhausServlet extends HttpServlet {
         return dauer;
     }
     
-    private void SlotStatistiken(String slot, String key) {
-		String s = slot;
-		int max = 0;
-		String str;
-		while ("" != slot) {
-			if (slot.indexOf(",") == -1) {
-				str = slot.substring(20, slot.length() - 1);
-				slot = "";
-			} else {
-
-				str = slot.substring(20, slot.indexOf(",") - 1);
-				slot = slot.substring(slot.indexOf(",") + 2);
-			}
-
-			int j = Integer.parseInt(str);
-			if (j > max) {
-				max = j;
-			}
-		}
-		Slot = new int[max + 1];
-		for (int i = 0; i <= max; i++) {
-			Slot[i] = 0;
-		}
-
-		while ("" != s) {
-			if (s.indexOf(",") == -1) {
-				str = s.substring(20, s.length() - 1);
-				s = "";
-			} else {
-
-				str = s.substring(20, s.indexOf(",") - 1);
-				s = s.substring(s.indexOf(",") + 2);
-			}
-			int j = Integer.parseInt(str);
-
-			if (key.indexOf(",") == -1) {
-
-				str = key;
-				key = "";
-			} else {
-
-				str = key.substring(0, key.indexOf(","));
-				key = key.substring(key.indexOf(",") + 1);
-			}
-
-			Slot[j] = Integer.parseInt(str);
-		}
-
-	}
-
+   
     private Float getPersistentteuerstesTicket() {
 
         ServletContext application = getApplication();
